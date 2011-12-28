39978332f27d57d7c13889a1c11d8f4dd853bec8
Jim Holmes <Jim.Holmes@Telerik.com>
2011-12-28 00:22:39 -0500

    Lame commit message. Sue me.

--- 31Days-FunctionalTestIntro/WebDriver/AjaxIssues.cs
+++ 31Days-FunctionalTestIntro/WebDriver/AjaxIssues.cs
@@ -85,59 +85,81 @@ namespace WebDriverDemo
         [Test]
         public void Working_with_no_content_data_driven()
         {
-            IList<Car> cars = new List<Car>
-            {
-                new Car { Make = "Acura", Model = "Integra", Color = "Sea Green", Message = "Sea Green Acura Integra" },
-                new Car { Make = "Audi", Model = "S4", Color = "Metallic", Message = "Metallic Audi S4" },
-                new Car { Make = "BMW", Model = "7 series", Color = "Brown", Message = "Brown BMW 7 series" }
-            };
+            IList<Car> cars = CarFactory.Return_three_valid_cars();
 
             browser.Navigate().GoToUrl(
                     "http://www.asp.net/ajaxLibrary/AjaxControlToolkitSampleSite/CascadingDropDown/CascadingDropDown.aspx");
             WebDriverWait wait = new WebDriverWait(browser, TimeSpan.FromSeconds(10));
 
-            foreach(Car car in cars)
+            foreach (Car car in cars)
             {
                 browser.Navigate().Refresh();
             
-                var listOfMakes = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList1"));
-
-                wait.Until<IWebElement>((d) =>
-                {
-                    return d.FindElement(By.XPath(
-                                                  "id('ctl00_SampleContent_DropDownList1')/option[text()='" + car.Make + "']"));
-                });
-                var makeOptions = new SelectElement(listOfMakes);
-                makeOptions.SelectByText(car.Make);
-
-                var listOfModels = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList2"));
-                wait.Until<IWebElement>((d) =>
-                {
-                    return d.FindElement(By.XPath(
-                                                  "id('ctl00_SampleContent_DropDownList2')/option[text()='" + car.Model + "']"));
-                });
-                var modelOptions = new SelectElement(listOfModels);
-                modelOptions.SelectByText(car.Model);
-
-                var listOfColors = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList3"));
-                wait.Until<IWebElement>((d) =>
-                {
-                    return d.FindElement(By.XPath(
-                                                  "id('ctl00_SampleContent_DropDownList3')/option[text()='" + car.Color + "']"));
-                });
-                var colorOptions = new SelectElement(listOfColors);
-                colorOptions.SelectByText(car.Color);
-
-                
-                //var messageActual = browser.FindElement(By.Id("ctl00_SampleContent_Label1"));
-                var messageActual = wait.Until<IWebElement>((d) =>
-                {
-                    return d.FindElement(By.XPath(
-                                                  "id('ctl00_SampleContent_Label1')[contains(.,'" + 
-                                                  car.Message + "')]"));
-                });
-                Assert.IsTrue(messageActual.Text.Contains(car.Message),"Message: " + messageActual.Text);
+                Select_make(car, wait);
+                Select_model(car, wait);
+                Select_color(car, wait);
+                Validate_message(car, wait);
             }
+        }
+  
+        private void Validate_message(Car car, WebDriverWait wait)
+        {
+            var messageActual = wait.Until<IWebElement>((d) =>
+            {
+                return d.FindElement(By.XPath(
+                                              "id('ctl00_SampleContent_Label1')[contains(.,'" +
+                                              car.Message + "')]"));
+            });
+            Assert.IsTrue(messageActual.Text.Contains(car.Message), "Message: " + messageActual.Text);
+        }
+  
+        private void Select_color(Car car, WebDriverWait wait)
+        {
+            var listOfColors = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList3"));
+            wait.Until<IWebElement>((d) =>
+            {
+                return d.FindElement(By.XPath(
+                                              "id('ctl00_SampleContent_DropDownList3')/option[text()='" + car.Color + "']"));
+            });
+            var colorOptions = new SelectElement(listOfColors);
+            colorOptions.SelectByText(car.Color);
+        }
+  
+        private void Select_model(Car car, WebDriverWait wait)
+        {
+            var listOfModels = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList2"));
+            wait.Until<IWebElement>((d) =>
+            {
+                return d.FindElement(By.XPath(
+                                              "id('ctl00_SampleContent_DropDownList2')/option[text()='" + car.Model + "']"));
+            });
+            var modelOptions = new SelectElement(listOfModels);
+            modelOptions.SelectByText(car.Model);
+        }
+  
+        private void Select_make(Car car, WebDriverWait wait)
+        {
+            var listOfMakes = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList1"));
+            wait.Until<IWebElement>((d) =>
+            {
+                return d.FindElement(By.XPath(
+                                              "id('ctl00_SampleContent_DropDownList1')/option[text()='" + car.Make + "']"));
+            });
+            var makeOptions = new SelectElement(listOfMakes);
+            makeOptions.SelectByText(car.Make);
+        }
+    }
+
+    public static class CarFactory
+    {
+        public static IList<Car> Return_three_valid_cars()
+        {
+            return new List<Car>
+            {
+                new Car { Make = "Acura", Model = "Integra", Color = "Sea Green", Message = "Sea Green Acura Integra" },
+                new Car { Make = "Audi", Model = "S4", Color = "Metallic", Message = "Metallic Audi S4" },
+                new Car { Make = "BMW", Model = "7 series", Color = "Brown", Message = "Brown BMW 7 series" }
+            };
         }
     }
 
