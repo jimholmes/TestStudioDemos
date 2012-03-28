9b5fa4330cf0bd033ac8a7f21dee0cfe71b50c86
Jim Holmes <Jim.Holmes@Telerik.com>
2012-03-27 23:38:16 -0400

    Refactored explicit wait example for clarity, renamed tests also for clarity.

--- 31Days-FunctionalTestIntro/WebDriver/AjaxIssues.cs
+++ 31Days-FunctionalTestIntro/WebDriver/AjaxIssues.cs
@@ -29,14 +29,19 @@ namespace WebDriverDemo
         }
 
         [Test]
-        public void Working_with_no_element()
+        public void Implicit_wait_for_element_to_appear()
         {
             IWebDriver browser = new FirefoxDriver();
             browser.Navigate().GoToUrl(
                 "http://localhost/AJAXDemo/DropDown/DropDown.aspx");
-            browser.FindElement(By.Id("ctl00_SampleContent_TextLabel")).Click();
-            browser.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
-            browser.FindElement(By.Id("ctl00_SampleContent_Option1")).Click();
+            browser.FindElement(
+                By.Id("ctl00_SampleContent_TextLabel")).Click();
+            
+            browser.Manage().Timeouts().
+                ImplicitlyWait(TimeSpan.FromSeconds(10));
+            
+            browser.FindElement(
+                By.Id("ctl00_SampleContent_Option1")).Click();
             
             Assert.IsTrue(browser.FindElement(By.Id("ctl00_SampleContent_lblSelection"))
                                  .Text
@@ -45,7 +50,7 @@ namespace WebDriverDemo
         }
 
         [Test]
-        public void Working_with_no_content()
+        public void Explicit_wait_for_content_to_appear_in_existing_element()
         {
             string make = "Acura";
             string model = "Integra";
@@ -56,35 +61,65 @@ namespace WebDriverDemo
             var listOfMakes = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList1"));
 
             WebDriverWait wait = new WebDriverWait(browser, TimeSpan.FromSeconds(10));
-            wait.Until<IWebElement>((d) =>
-            {
-                return d.FindElement(By.XPath(
-                    "id('ctl00_SampleContent_DropDownList1')/option[text()='Acura']"));
-            });
-                                //"id('ctl00_SampleContent_DropDownList1')/option[text()='" + make + "']"));
-            var makeOptions = new SelectElement(listOfMakes);
-            makeOptions.SelectByText(make);
+            
+            Wait_for_make_option_to_populate(wait, make);
+            Select_make_from_list(listOfMakes, make);
 
             var listOfModels = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList2"));
-            wait.Until<IWebElement>((d) =>
-            {
-                return d.FindElement(By.XPath(
-                    "id('ctl00_SampleContent_DropDownList2')/option[text()='" + model + "']"));
-            });
-            var modelOptions = new SelectElement(listOfModels);
-            modelOptions.SelectByText(model);
+            Wait_for_model_option_to_populate(model, wait);
+            Select_model_from_list(listOfModels, model);
 
             var listOfColors = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList3"));
+            Wait_for_color_option_to_populate(color, wait);
+            Select_color_from_list(listOfColors, color);
+
+            browser.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            Assert.IsTrue(browser.FindElement(By.Id("ctl00_SampleContent_Label1")).Displayed);
+        }
+  
+        private void Select_color_from_list(IWebElement listOfColors, string color)
+        {
+            var colorOptions = new SelectElement(listOfColors);
+            colorOptions.SelectByText(color);
+        }
+  
+        private void Select_model_from_list(IWebElement listOfModels, string model)
+        {
+            var modelOptions = new SelectElement(listOfModels);
+            modelOptions.SelectByText(model);
+        }
+  
+        private void Select_make_from_list(IWebElement listOfMakes, string make)
+        {
+            var makeOptions = new SelectElement(listOfMakes);
+            makeOptions.SelectByText(make);
+        }
+  
+        private void Wait_for_color_option_to_populate(string color, WebDriverWait wait)
+        {
             wait.Until<IWebElement>((d) =>
             {
                 return d.FindElement(By.XPath(
                     "id('ctl00_SampleContent_DropDownList3')/option[text()='" + color + "']"));
             });
-            var colorOptions = new SelectElement(listOfColors);
-            colorOptions.SelectByText(color);
-
-            browser.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
-            Assert.IsTrue(browser.FindElement(By.Id("ctl00_SampleContent_Label1")).Displayed);
+        }
+  
+        private void Wait_for_model_option_to_populate(string model, WebDriverWait wait)
+        {
+            wait.Until<IWebElement>((d) =>
+            {
+                return d.FindElement(By.XPath(
+                    "id('ctl00_SampleContent_DropDownList2')/option[text()='" + model + "']"));
+            });
+        }
+  
+        private void Wait_for_make_option_to_populate(WebDriverWait wait, string make)
+        {
+            wait.Until<IWebElement>((d) =>
+            {
+                return d.FindElement(By.XPath(
+                    "id('ctl00_SampleContent_DropDownList1')/option[text()='"+ make + "']"));
+            });
         }
 
         [Test]
